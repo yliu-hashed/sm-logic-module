935b51245b15d7600d81e24d504256a60f2ddf00
YLIU <72162739+yliu-hashed@users.noreply.github.com>
2025-11-14 14:01:48 -0500

    Add test case for div_seq.v

--- src/div_seq.v
+++ src/div_seq.v
@@ -9,24 +9,25 @@ module top(
   (* color = "white" *) input  START,
   (* color = "green" *) output DONE,
 
-  input  [`LEN-1:0] A, // numerator
-  input  [`LEN-1:0] B, // denominator
-  output [`LEN-1:0] Q, // quotient
-  output [`LEN-1:0] R  // remainder
+  input  [LEN-1:0] A, // numerator
+  input  [LEN-1:0] B, // denominator
+  output [LEN-1:0] Q, // quotient
+  output [LEN-1:0] R  // remainder
   );
 
-  localparam SLEN = $clog2(`LEN);
+  parameter LEN = `LEN;
+  localparam SLEN = $clog2(LEN);
 
   reg [SLEN:0] state = 0;
 
-  reg [`LEN-1:0] argD  = 0; // denominator
-  reg [`LEN-1:0] tmpNQ = 0; // shared numerator and quotient
-  reg [`LEN-1:0] tmpR  = 0; // remainder
+  reg [LEN-1:0] argD  = 0; // denominator
+  reg [LEN-1:0] tmpNQ = 0; // shared numerator and quotient
+  reg [LEN-1:0] tmpR  = 0; // remainder
 
   assign Q = tmpNQ;
   assign R = tmpR;
 
-  wire [`LEN-1:0] nxR = (tmpR << 1) | tmpNQ[`LEN-1];
+  wire [LEN-1:0] nxR = (tmpR << 1) | tmpNQ[LEN-1];
   wire div = nxR >= argD;
   wire done = !state;
   assign DONE = done;
---
+++ tb/tb_div_seq.v
@@ -0,0 +1,87 @@
+// Test the correctness of the div_seq.v module
+`include "src/div_seq.v"
+`default_nettype none
+
+module tb;
+
+  parameter LEN = 16;
+
+  localparam bad_word = {LEN{1'bX}};
+
+  reg  CLK   = 0;
+  reg  START = 0;
+  wire DONE;
+
+  reg  [LEN-1:0] A = bad_word;
+  reg  [LEN-1:0] B = bad_word;
+  wire [LEN-1:0] Q;
+  wire [LEN-1:0] R;
+
+  top #(
+    .LEN(LEN)
+  ) inst ( .* );
+
+  integer wait_count;
+
+  task tick ();
+    begin
+      CLK <= 0; #1;
+      wait_count = wait_count + 1;
+      CLK <= 1; #1;
+    end
+  endtask
+
+  task work (
+    input [LEN-1:0] a,
+    input [LEN-1:0] b,
+    input [LEN-1:0] q,
+    input [LEN-1:0] r,
+    input integer timeout
+    );
+    begin
+      wait_count = 0;
+      // feed in data to be processed
+      START <= 1;
+      A <= a;
+      B <= b;
+      tick();
+
+      START <= 0;
+      A <= bad_word;
+      B <= bad_word;
+      tick();
+
+      // wait until done
+      while (!DONE) begin
+        if (wait_count > timeout) begin
+          $display("[FAIL] timeout for longer than %0d cycles", timeout);
+          $stop;
+        end
+        tick();
+      end
+
+      // check result
+      if (Q !== q) begin
+        $display("[FAIL] mismatch quotient 0x%4h from expectation 0x%4h after %0d cycles", Q, q, wait_count);
+        $stop;
+      end
+      if (R !== r) begin
+        $display("[FAIL] mismatch remainder 0x%4h from expectation 0x%4h after %0d cycles", R, r, wait_count);
+        $stop;
+      end
+    end
+  endtask
+
+  integer i;
+  wire [LEN-1:0] hash_a = i * 193;
+  wire [LEN-1:0] hash_b = i * 1543;
+  wire [LEN-1:0] hash_q = (hash_b != 0) ? (hash_a / hash_b) : {LEN{1'b1}};
+  wire [LEN-1:0] hash_r = (hash_b != 0) ? (hash_a % hash_b) : hash_a;
+
+  initial begin
+    for (i = 0; i < 100; i = i + 1) begin
+      work ( hash_a, hash_b, hash_q, hash_r, 16 );
+    end
+    $display("[PASS] tb_div_seq.v");
+  end
+endmodule
